487cd316433e4d08e1b3507223e1f0d8480a1957
DeaconRodda <deacon@sqglz.com>
2019-08-21 17:20:43 -0600

    hello world

---
+++ sqgopt.js
@@ -0,0 +1,10 @@
+
+
+// const sqgopt = () => {
+// 	console.log("When this project is developed there will be no more excuse for aggressive permission gathering elements on landing pages.")
+// };
+
+
+var sqgopt = function() {
+	console.log("When this project is developed there will be no more excuse for aggressive permission gathering elements on landing pages.")
+};
