6f798a352410ae26b261970b68018e3766883dda
DeaconRodda <deacon@sqglz.com>
2019-08-26 09:51:25 -0600

    basic functionality about half finished.

--- sqgopt.js
+++ sqgopt.js
@@ -1,10 +1,13 @@
 
 
-// const sqgopt = () => {
-// 	console.log("When this project is developed there will be no more excuse for aggressive permission gathering elements on landing pages.")
-// };
-
-
-var sqgopt = function() {
+const sqgopt = function() {
 	console.log("When this project is developed there will be no more excuse for aggressive permission gathering elements on landing pages.")
+};
+
+
+const findCTA = function() {
+	ctaBox = sqgOptCTABox.getBoundingClientRect();
+	console.log(ctaBox.top);
+
+	
 };
